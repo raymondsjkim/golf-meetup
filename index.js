d75f92c0974bbaca06f65ab893982cc370b07a8f
raymondsjkim <raymond.sj.kim@gmail.com>
2020-07-19 11:35:29 -0700

    eslint and prettier

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-//get express and create routes
 const express = require('express');
 const path = require('path');
-const { response } = require('express');
 
 const app = express();
 
 const port = 4000;
 
-
-//serve static files 
-//app.use is how middlewhere is applied in express
-//instruct express to look into the static folder for each request it receives
-//and if it finds a matching file, it will send it to the browser
 app.use(express.static(path.join(__dirname, './view')));
 
-//
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './view/index.html'));
+  response.sendFile(path.join(__dirname, './view/index.html'));
 });
 
-
-
-
-
 app.listen(port, () => {
-    console.log(`Express server listening on port: ${port}`);
-})
+  // eslint-disable-next-line no-console
+  console.log(`Express server listening on port: ${port}`);
+});
