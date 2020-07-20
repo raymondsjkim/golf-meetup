c10f259928e0db2395c0261d4a5be8228887078b
raymondsjkim <raymond.sj.kim@gmail.com>
2020-07-20 14:20:07 -0700

    update nodemon

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
-const app = express();
 
+const app = express();
 const port = 4000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use('/', routes());
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
